ce50eb2536ed5c6d0ab2fcaf4837ce74aa07fc3c
SAmbadipudi <vmsrikanth.a@gmail.com>
2019-09-21 09:32:39 +0000

    khfk

--- cloudwatch-event/cloudwatch-event-target.tf
+++ cloudwatch-event/cloudwatch-event-target.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_target" "market_cloudwatch_event_target" {
   count     = "${length("${var.event_rules}")}" 
   target_id = "${var.event_target_id}-${var.event_rules[count.index]}"
-  rule      = ["${var.event_taget_rule_name[count.index]}"]
+  rule      = "${var.event_taget_rule_name[count.index]}"
   arn       = "${var.event_target_resource_arn}"
   input     = <<EOF
 { 
