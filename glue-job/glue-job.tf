d24a1989277027f4c07fecfa9714ce950e6cf483
SAmbadipudi <45809793+SAmbadipudi@users.noreply.github.com>
2019-09-22 16:30:25 +0530

    poitx

--- glue-job/glue-job.tf
+++ glue-job/glue-job.tf
@@ -17,4 +17,18 @@ resource "aws_glue_job" "uki-dev-glue-mi4cc-avaya" {
     "--extra-jars" = "${var.extra-jars}"
     "--encryption-type" = "${var.encryption-type}"
   }
+ tags = {
+      Name                = "${var.jobnames[count.index]}"
+      Owner-CreatedBy	  = "${var.owner-createdby}"
+      Owner-Data	  = "${var.owner-data}"
+      Owner-Infra         = "${var.owner-infra}"
+      Project-Name        = "${var.project-name}"
+      Project-SubProjectName = "${var.project-subprojectname}"
+      Project-WBSCode	  = "${var.project-wbscode}"
+      TechnicalOwner      = "${var.technical_owner}"
+      Environment         = "${lower(terraform.workspace)}"
+      BillingCode         = "${var.billing_code}"
+      Compliance          = "${var.compliance}"
+    }
+
 }
