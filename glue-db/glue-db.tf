e9c1ea40d22a6812c955f25f00f0aaf55decec50
SAmbadipudi <45809793+SAmbadipudi@users.noreply.github.com>
2019-09-22 16:21:14 +0530

    pqoowqowq

--- glue-db/glue-db.tf
+++ glue-db/glue-db.tf
@@ -16,12 +16,12 @@ resource "aws_glue_connection" "uki-dev-glue-con-mi4cc-avaya" {
   
   tags = {
       Name                = "${var.cloudwatch_event_rule_name}"
-      Owner-CreatedBy	  = "${var.Owner-CreatedBy}"
-      Owner-Data	  = "${var.Owner-Data}"
-      Owner-Infra         = "${var.Owner-Infra}"
-      Project-Name        = "${var.Project-Name}"
-      Project-SubProjectName = "${var.Project-SubProjectName}"
-      Project-WBSCode	  = "${var.Project-WBSCode}"
+      Owner-CreatedBy	  = "${var.owner-createdby}"
+      Owner-Data	  = "${var.owner-data}"
+      Owner-Infra         = "${var.owner-infra}"
+      Project-Name        = "${var.project-name}"
+      Project-SubProjectName = "${var.project-subprojectname}"
+      Project-WBSCode	  = "${var.project-wbscode}"
       TechnicalOwner      = "${var.technical_owner}"
       Environment         = "${lower(terraform.workspace)}"
       BillingCode         = "${var.billing_code}"
