4163b5e91b0f34df001782a689ae98339179cfa0
SAmbadipudi <45809793+SAmbadipudi@users.noreply.github.com>
2019-09-22 16:29:27 +0530

    poiuu

--- glue-db/glue-db.tf
+++ glue-db/glue-db.tf
@@ -7,24 +7,10 @@ resource "aws_glue_connection" "uki-dev-glue-con-mi4cc-avaya" {
   }
 
   name = "${var.glue-conn-name}"
-
   physical_connection_requirements {
     availability_zone      = "${var.avail-zone}"
     security_group_id_list = "${var.seg-id}" 
     subnet_id              = "${var.sub-id}"
   }
   
-  tags = {
-      Name                = "${var.glue-conn-name}"
-      Owner-CreatedBy	  = "${var.owner-createdby}"
-      Owner-Data	  = "${var.owner-data}"
-      Owner-Infra         = "${var.owner-infra}"
-      Project-Name        = "${var.project-name}"
-      Project-SubProjectName = "${var.project-subprojectname}"
-      Project-WBSCode	  = "${var.project-wbscode}"
-      TechnicalOwner      = "${var.technical_owner}"
-      Environment         = "${lower(terraform.workspace)}"
-      BillingCode         = "${var.billing_code}"
-      Compliance          = "${var.compliance}"
-    }
 }
